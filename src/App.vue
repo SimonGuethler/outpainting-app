3f1d841ba0d96722102610e9d0889b021aea7129
Simon Güthler <sg811@web.de>
2023-05-23 00:36:44 +0200

    Add keyboard shortcuts

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-<!--    <div id="app">-->
-        <RouterView/>
-<!--    </div>-->
+    <RouterView/>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@1,200&display=swap');
+/*@import url('https://fonts.googleapis.com/css2?family=Newsreader:wght@200&display=swap');*/
+/*@import url('https://fonts.googleapis.com/css2?family=Newsreader&display=swap');*/
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,34 @@
              class="item" style="display: flex; flex-direction: column; align-items: center"
         >
             <va-image
+                :class="{imageBorder: showBorder}"
                 :src="item.data"
                 :alt="item.prompt"
                 :title="item.prompt"
                 loading="lazy"
                 style="height: 75vh; width: 75vh; pointer-events: none;"
             />
-            <div style="font-size: 1.3rem; margin-top: 1rem; color: antiquewhite">
+            <div class="text">
                 {{ item.prompt }}
             </div>
         </div>
     </main>
+    <div v-if="showDevBar" class="bar">
+        <va-button
+            class="button"
+            color="danger"
+            @click="reset"
+        >
+            Reset
+        </va-button>
+        <va-button
+            class="button"
+            color="dark"
+            @click="generateImage"
+        >
+            Generate
+        </va-button>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -24,8 +41,11 @@ import axios from 'axios';
 const loading = ref<boolean>(true);
 const prompt = ref<string>('');
 const images = reactive<{ [key: string]: string }>({});
+const imageCount = ref<number>(0);
 
-const imagesArray = computed<string[]>(() => Object.entries(images).map(([, value]) => value));
+const showDevBar = ref<boolean>(false);
+const showBorder = ref<boolean>(false);
+
 const promptArray = computed<string[]>(() =>
     prompt.value.split('\n').map((line) => line.trim()).filter((line) => line.length > 0)
 );
@@ -35,8 +55,6 @@ const dataArray = computed<{ prompt: string, data: string }[]>(() => {
         data: value,
     }));
 });
-
-const windowWidth = computed<number>(() => window.innerWidth);
 
 const axiosInstance = axios.create({
     baseURL: 'http://10.40.2.30:8000',
@@ -89,7 +107,10 @@ const getImageCount = async (): Promise<number | undefined> => {
     })
     if (!response || !response.data) return;
 
-    return Number(response.data);
+    const count = Number(response.data);
+    imageCount.value = count;
+
+    return count;
 }
 
 const loadPrompts = async () => {
@@ -98,8 +119,7 @@ const loadPrompts = async () => {
     if (!response || !response.data) return;
 
     const prompts: string[] = [];
-    for (let [key, value] of response.data.entries()) {
-        // prompts.push(key + 1 + ' | ' + value + '\n');
+    for (let [_, value] of response.data.entries()) {
         prompts.push(value);
     }
     prompt.value = prompts.join('\n');
@@ -115,7 +135,7 @@ const resetVariables = () => {
     prompt.value = '';
 }
 
-const horizontalScroll = () => {
+const enableHorizontalScroll = () => {
     const scrollable = document.getElementById("scrollable");
     scrollable?.addEventListener("wheel", (event) => {
         event.preventDefault();
@@ -130,6 +150,70 @@ const scrollToRight = () => {
     scrollable.scrollLeft = scrollable.scrollWidth;
 }
 
+const scrollToLeft = () => {
+    const scrollable = document.getElementById("scrollable");
+    if (!scrollable) return;
+    scrollable.scrollLeft = 0;
+}
+
+enum ScrollDirection {
+    Left,
+    Right,
+}
+
+let requestId = 0;
+
+const startScroll = (direction: ScrollDirection) => {
+    if (requestId) return;
+
+    const container = document.getElementById("scrollable")!;
+    const scrollAmount = 0.25;
+
+    const scroll = () => {
+        if (direction === ScrollDirection.Left) {
+            container.scrollLeft -= scrollAmount;
+        } else if (direction === ScrollDirection.Right) {
+            container.scrollLeft += scrollAmount;
+        }
+        requestId = requestAnimationFrame(scroll);
+    }
+
+    scroll();
+}
+
+function stopScroll() {
+    cancelAnimationFrame(requestId);
+    requestId = 0;
+}
+
+const addKeyShortcuts = () => {
+    const handleKeyPress = (event: { keyCode: number; }) => {
+        if (event.keyCode === 84) { // T
+            showDevBar.value = !showDevBar.value;
+        }
+        if (event.keyCode === 81) { // Q
+            scrollToLeft();
+        }
+        if (event.keyCode === 69) { // E
+            scrollToRight();
+        }
+        if (event.keyCode === 65) { // A
+            startScroll(ScrollDirection.Left);
+        }
+        if (event.keyCode === 68) { // D
+            startScroll(ScrollDirection.Right);
+        }
+        if (event.keyCode === 83) { // S
+            stopScroll();
+        }
+        if (event.keyCode === 71) { // G
+            showBorder.value = !showBorder.value;
+        }
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+}
+
 const init = async () => {
     await Promise.all([
         loadPrompts(),
@@ -140,7 +224,8 @@ const init = async () => {
 onMounted(async () => {
     resetVariables();
     await init();
-    horizontalScroll();
+    enableHorizontalScroll();
+    addKeyShortcuts();
 });
 
 </script>
@@ -157,6 +242,39 @@ onMounted(async () => {
 .item {
     height: 100%;
     width: 75vh;
+}
+
+.text {
+    font-size: 2.5rem;
+    padding-inline: 2rem;
+    margin-top: 2rem;
+    color: antiquewhite;
+    white-space: pre-wrap;
+    text-align: center;
+    line-height: 1.25;
+    font-family: 'Newsreader', serif;
+}
+
+.bar {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 0.5rem;
+    border: 1px solid antiquewhite;
+    border-bottom: none;
+    border-radius: 10px 10px 0 0;
+}
+
+.button {
+    height: 2rem;
+    width: 7rem;
+    margin-inline: 0.25rem;
+    --va-button-font-size: 1.25rem;
+}
+
+.imageBorder {
+    border: 1px solid red;
 }
 
 </style>
