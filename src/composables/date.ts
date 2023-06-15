59b35f2cbdcb5a10ad8450fd23085d3e18a19f6e
Simon Güthler <sg811@web.de>
2023-06-16 00:01:15 +0200

    Fix date parsing for years

--- src/composables/date.ts
+++ src/composables/date.ts
@@ -2,5 +2,7 @@ import { DateTime } from 'luxon';
 
 export const formatDate = (timestamp: string) => {
     const dateTime = DateTime.fromISO(timestamp);
+    if (!dateTime.isValid) return timestamp;
+    if (dateTime.toFormat('dd.MM.yyyy') === '01.01.' + dateTime.toFormat('yyyy')) return dateTime.toFormat('yyyy');
     return dateTime.toFormat('dd.MM.yyyy HH:mm');
 }
